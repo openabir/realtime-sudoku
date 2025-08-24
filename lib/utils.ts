44de2e5135a71c490b4e0d6de65c7eed8761b1b7
openabir <heyyabir@gmail.com>
2025-08-25 00:29:44 +0530

    feat: Add sudoku generation and utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,38 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { LiveList, LiveObject } from "@liveblocks/client";
+import { type ClassValue, clsx } from "clsx";
+import { env } from "process";
+import { twMerge } from "tailwind-merge";
+import { getSudoku } from "./sudoku";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export const randomId = (count: number) => {
+  return "x".repeat(count).replace(/[x]/g, () => {
+    return ((Math.random() * 16) | 0).toString(16);
+  });
+};
+
+export const generateSudoku = (difficulty: string) => {
+  const sudokuGame = getSudoku();
+  const str = sudokuGame.generate(difficulty);
+  const solved: any = sudokuGame.solve(str);
+
+  const sudokuGrid = new LiveList<Cell>([]);
+
+  str.split("").forEach((value: string, index: number) => {
+    const square = new LiveObject({
+      value: value === "." ? 0 : Number(value),
+      immutable: value === "." ? false : true,
+      valid: value === "." ? false : true,
+      key: value === "." ? Number(solved[index]) : Number(value),
+    });
+    sudokuGrid.push(square);
+  });
+
+  return sudokuGrid;
+};
+
+export function absoluteUrl(path: string) {
+  return `${env.NEXT_PUBLIC_APP_URL}${path}`;
 }
